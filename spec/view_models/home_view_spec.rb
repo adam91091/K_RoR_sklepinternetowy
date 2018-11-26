1db80ddd042555bc750b1c6a158fbd5c85383c65
BeataSzczuka <beata.szczuka@gmail.com>
2018-11-26 18:18:04 +0100

    Fix tests for HomeView

--- spec/view_models/home_view_spec.rb
+++ spec/view_models/home_view_spec.rb
@@ -1,31 +1,100 @@
 require 'rails_helper'
 
 RSpec.describe HomeView do
-  home_view = HomeView.new
-  women = Category.where(name: 'Women')
-  women_ids = Category.where(parent: women).pluck(:id)
-  men = Category.where(name: 'Men')
-  men_ids = Category.where(parent: men).pluck(:id)
+
+  let!(:women_category) { FactoryBot.create :category, name: 'Women', main_women_category: true}
+  let!(:jeans_category) { FactoryBot.create :category, name: 'Jeans', parent: women_category.id}
+
+  let!(:men_category) { FactoryBot.create :category, name: 'Men', main_men_category: true}
+  let!(:tshirts_category) { FactoryBot.create :category, name: 'T-shirts', parent: men_category.id}
+
+  let!(:home_view) { FactoryBot.build :home_view}
+
   describe '#new_products_women' do
-    it 'should return the last three women products' do
-      home_view.new_products_women.should == Product.where(category_id: women_ids).last(3)
+
+    context 'there is only one product for women' do
+
+      let!(:product1) { FactoryBot.create :product, name: 'Product1', category: jeans_category}
+
+      it 'should return the only product for women' do
+        expect(home_view.new_products_women).to eq([product1])
+      end
+
     end
-    it 'should return the only two women products' do
-      home_view.new_products_women.should == Product.where(category_id: women_ids).first(2)
+
+    context 'there are many diffrent products' do
+
+      let!(:product1) { FactoryBot.create :product, name: 'Product1', category: jeans_category}
+      let!(:product2) { FactoryBot.create :product, name: 'Product2', category: tshirts_category}
+      let!(:product3) { FactoryBot.create :product, name: 'Product3', category: tshirts_category}
+      let!(:product4) { FactoryBot.create :product, name: 'Product4', category: tshirts_category}
+      let!(:product5) { FactoryBot.create :product, name: 'Product5', category: jeans_category}
+      let!(:product6) { FactoryBot.create :product, name: 'Product6', category: jeans_category}
+      let!(:product7) { FactoryBot.create :product, name: 'Product7', category: jeans_category}
+      let!(:product8) { FactoryBot.create :product, name: 'Product8', category: jeans_category}
+
+      it 'should return the last three women products' do
+        products = [product1];
+        expect(home_view.new_products_women).to eq([product6, product7, product8])
+      end
+
     end
-    it 'should return empty list' do
-      home_view.new_products_women.should == []
+
+    context 'there is no product for woman' do
+
+      let!(:product1) { FactoryBot.create :product, name: 'Product2', category: tshirts_category}
+      let!(:product2) { FactoryBot.create :product, name: 'Product2', category: tshirts_category}
+      let!(:product3) { FactoryBot.create :product, name: 'Product3', category: tshirts_category}
+      let!(:product4) { FactoryBot.create :product, name: 'Product4', category: tshirts_category}
+
+      it 'should return empty list' do
+        expect(home_view.new_products_women).to eq([])
+      end
+
     end
   end
+
   describe '#new_products_men' do
-    it 'should return the last three men products' do
-      home_view.new_products_men.should == Product.where(category_id: men_ids).last(3)
+  
+    context 'there is only one product for men' do
+
+      let!(:product1) { FactoryBot.create :product, name: 'Product1', category: tshirts_category}
+
+      it 'should return the only product for men' do
+        expect(home_view.new_products_men).to eq([product1])
+      end
+
     end
-    it 'should return the only two men products' do
-      home_view.new_products_men.should == Product.where(category_id: men_ids).first(2)
+
+    context 'there many diffrent products' do
+
+      let!(:product1) { FactoryBot.create :product, name: 'Product1', category: jeans_category}
+      let!(:product2) { FactoryBot.create :product, name: 'Product2', category: tshirts_category}
+      let!(:product3) { FactoryBot.create :product, name: 'Product3', category: tshirts_category}
+      let!(:product4) { FactoryBot.create :product, name: 'Product4', category: tshirts_category}
+      let!(:product5) { FactoryBot.create :product, name: 'Product5', category: jeans_category}
+      let!(:product6) { FactoryBot.create :product, name: 'Product6', category: jeans_category}
+      let!(:product7) { FactoryBot.create :product, name: 'Product7', category: jeans_category}
+      let!(:product8) { FactoryBot.create :product, name: 'Product8', category: tshirts_category}
+
+      it 'should return the last three products for men' do
+        products = [product1];
+        expect(home_view.new_products_men).to eq([product3, product4, product8])
+      end
+
     end
-    it 'should return empty list' do
-      home_view.new_products_men.should == []
+
+    context 'there is no product for men' do
+
+      let!(:product1) { FactoryBot.create :product, name: 'Product2', category: jeans_category}
+      let!(:product2) { FactoryBot.create :product, name: 'Product2', category: jeans_category}
+      let!(:product3) { FactoryBot.create :product, name: 'Product3', category: jeans_category}
+      let!(:product4) { FactoryBot.create :product, name: 'Product4', category: jeans_category}
+
+      it 'should return empty list' do
+        expect(home_view.new_products_men).to eq([])
+      end
+
     end
   end
 end
