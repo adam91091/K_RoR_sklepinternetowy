518e3a18e7b7286e9d48cbebdf91ba0ce9db0750
chevrette <denkiewicz.karolina@gmail.com>
2018-12-03 00:24:24 +0100

    Run rubocop

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,20 +1,20 @@
 class Category < ApplicationRecord
-	has_many :products
-	has_many :child_categories, :class_name => "Category", :foreign_key => 'parent'
+  has_many :products
+  has_many :child_categories, class_name: 'Category', foreign_key: 'parent'
 
-	def all_products
-		result = []
-		child_categories.each do |child|
-			result = result + child.all_products
-		end
-		return result + products
-	end
+  def all_products
+    result = []
+    child_categories.each do |child|
+      result += child.all_products
+    end
+    result + products
+  end
 
-	def all_concrete_products
-		result = []
-		all_products.each do |product|
-			result = product.all_concrete_products
-		end
-		return result
-	end
+  def all_concrete_products
+    result = []
+    all_products.each do |product|
+      result = product.all_concrete_products
+    end
+    result
+  end
 end
