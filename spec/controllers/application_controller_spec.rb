dfdcf6172e27217a206447c6ef908438af0dc9dd
BeataSzczuka <beata.szczuka@gmail.com>
2018-12-03 21:07:06 +0100

    Rubocop

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationController do
-
-    describe '#current_guest' do
-        it do
-            expect(subject.current_guest).to be_a(Guest)
-        end
+  describe '#current_guest' do
+    it do
+      expect(subject.current_guest).to be_a(Guest)
     end
-
+  end
 end
