0f8f5749c86cd045bd1c3cb0e05ed3e1305225f3
Adam <adam.kufel2@gmail.com>
2018-12-07 10:48:11 +0100

    Add_to_cart_tests_activated_fixed_cart_functionality

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe OrderItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:category1) { FactoryBot.create :category, name: 'Category1' }
+  let!(:product1) do
+    FactoryBot.create :product, name: 'Product1',
+                                category: category1
+  end
+  let!(:concrete) do
+    FactoryBot.create :concrete_product, product: product1,
+                                         price: 100.00, size: 'M', color: 'black', amount: 5
+  end
+  let!(:order_item) { FactoryBot.create :order_item, concrete_product: concrete }
+
+  context 'associations' do
+    it { should belong_to(:concrete_product) }
+  end
 end
