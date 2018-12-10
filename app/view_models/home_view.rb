0f1f7ea90a36bed9a730cc532a7bfdfd95dd5a8f
BeataSzczuka <beata.szczuka@gmail.com>
2018-12-10 19:09:04 +0100

    Change displaying the newest Products to ConcreteProducts

--- app/view_models/home_view.rb
+++ app/view_models/home_view.rb
@@ -2,12 +2,20 @@ class HomeView
   def new_products_women
     women = Category.where(name: 'Women')
     women_ids = Category.where(parent: women).pluck(:id)
-    Product.where(category_id: women_ids).last(3)
+    result = []
+    Product.where(category_id: women_ids).last(3).each do |product|
+      result += [product.all_concrete_products.last]
+    end
+    result
   end
 
   def new_products_men
     men = Category.where(name: 'Men')
     men_ids = Category.where(parent: men).pluck(:id)
-    Product.where(category_id: men_ids).last(3)
+    result = []
+    Product.where(category_id: men_ids).last(3).each do |product|
+      result += [product.all_concrete_products.last]
+    end
+    result
   end
 end
--- spec/view_models/home_view_spec.rb
+++ spec/view_models/home_view_spec.rb
@@ -12,9 +12,10 @@ RSpec.describe HomeView do
   describe '#new_products_women' do
     context 'there is only one product for women' do
       let!(:product1) { FactoryBot.create :product, name: 'Product1', category: jeans_category }
+      let!(:concrete1) { FactoryBot.create :concrete_product, product: product1, price: 100.00, size: 'S', color: '#000000', amount: 1 }
 
       it 'should return the only product for women' do
-        expect(home_view.new_products_women).to eq([product1])
+        expect(home_view.new_products_women).to eq([concrete1])
       end
     end
 
@@ -24,12 +25,17 @@ RSpec.describe HomeView do
       let!(:product3) { FactoryBot.create :product, name: 'Product3', category: tshirts_category }
       let!(:product4) { FactoryBot.create :product, name: 'Product4', category: tshirts_category }
       let!(:product5) { FactoryBot.create :product, name: 'Product5', category: jeans_category }
-      let!(:product6) { FactoryBot.create :product, name: 'Product6', category: jeans_category }
-      let!(:product7) { FactoryBot.create :product, name: 'Product7', category: jeans_category }
-      let!(:product8) { FactoryBot.create :product, name: 'Product8', category: jeans_category }
 
-      it 'should return the last three women products' do
-        expect(home_view.new_products_women).to eq([product6, product7, product8])
+      let!(:concrete1) { FactoryBot.create :concrete_product, product: product1, price: 106.00, size: 'S', color: '#000000', amount: 7 }
+      let!(:concrete2) { FactoryBot.create :concrete_product, product: product1, price: 105.00, size: 'S', color: '#000000', amount: 6 }
+      let!(:concrete3) { FactoryBot.create :concrete_product, product: product2, price: 104.00, size: 'S', color: '#000000', amount: 5 }
+      let!(:concrete4) { FactoryBot.create :concrete_product, product: product3, price: 103.00, size: 'S', color: '#000000', amount: 4 }
+      let!(:concrete5) { FactoryBot.create :concrete_product, product: product2, price: 102.00, size: 'S', color: '#000000', amount: 3 }
+      let!(:concrete6) { FactoryBot.create :concrete_product, product: product4, price: 101.00, size: 'S', color: '#000000', amount: 2 }
+      let!(:concrete7) { FactoryBot.create :concrete_product, product: product5, price: 100.00, size: 'S', color: '#000000', amount: 1 }
+
+      it 'should return two women products' do
+        expect(home_view.new_products_women).to eq([concrete1, concrete7])
       end
     end
 
@@ -48,9 +54,10 @@ RSpec.describe HomeView do
   describe '#new_products_men' do
     context 'there is only one product for men' do
       let!(:product1) { FactoryBot.create :product, name: 'Product1', category: tshirts_category }
+      let!(:concrete1) { FactoryBot.create :concrete_product, product: product1, price: 100.00, size: 'M', color: '#000000', amount: 1 }
 
       it 'should return the only product for men' do
-        expect(home_view.new_products_men).to eq([product1])
+        expect(home_view.new_products_men).to eq([concrete1])
       end
     end
 
@@ -59,21 +66,24 @@ RSpec.describe HomeView do
       let!(:product2) { FactoryBot.create :product, name: 'Product2', category: tshirts_category }
       let!(:product3) { FactoryBot.create :product, name: 'Product3', category: tshirts_category }
       let!(:product4) { FactoryBot.create :product, name: 'Product4', category: tshirts_category }
-      let!(:product5) { FactoryBot.create :product, name: 'Product5', category: jeans_category }
-      let!(:product6) { FactoryBot.create :product, name: 'Product6', category: jeans_category }
-      let!(:product7) { FactoryBot.create :product, name: 'Product7', category: jeans_category }
-      let!(:product8) { FactoryBot.create :product, name: 'Product8', category: tshirts_category }
+
+      let!(:concrete1) { FactoryBot.create :concrete_product, product: product1, price: 100.00, size: 'S', color: '#000000', amount: 1 }
+      let!(:concrete2) { FactoryBot.create :concrete_product, product: product2, price: 100.00, size: 'S', color: '#000000', amount: 2 }
+      let!(:concrete3) { FactoryBot.create :concrete_product, product: product2, price: 100.00, size: 'S', color: '#000000', amount: 3 }
+      let!(:concrete4) { FactoryBot.create :concrete_product, product: product3, price: 100.00, size: 'S', color: '#000000', amount: 4 }
+      let!(:concrete5) { FactoryBot.create :concrete_product, product: product4, price: 100.00, size: 'S', color: '#000000', amount: 5 }
 
       it 'should return the last three products for men' do
-        expect(home_view.new_products_men).to eq([product3, product4, product8])
+        expect(home_view.new_products_men).to eq([concrete2, concrete4, concrete5])
       end
     end
 
     context 'there is no product for men' do
       let!(:product1) { FactoryBot.create :product, name: 'Product2', category: jeans_category }
       let!(:product2) { FactoryBot.create :product, name: 'Product2', category: jeans_category }
-      let!(:product3) { FactoryBot.create :product, name: 'Product3', category: jeans_category }
-      let!(:product4) { FactoryBot.create :product, name: 'Product4', category: jeans_category }
+
+      let!(:concrete1) { FactoryBot.create :concrete_product, product: product2, price: 100.00, size: 'S', color: '#000000', amount: 5 }
+      let!(:concrete2) { FactoryBot.create :concrete_product, product: product1, price: 100.00, size: 'S', color: '#000000', amount: 5 }
 
       it 'should return empty list' do
         expect(home_view.new_products_men).to eq([])
