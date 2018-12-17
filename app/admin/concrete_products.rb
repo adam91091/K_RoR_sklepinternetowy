cb94583bdb757101080183cc47d01aa26ae3533a
chevrette <denkiewicz.karolina@gmail.com>
2018-12-17 20:55:46 +0100

    Add multiple images to concrete products

--- app/admin/concrete_products.rb
+++ app/admin/concrete_products.rb
@@ -1,6 +1,7 @@
 ActiveAdmin.register ConcreteProduct do
-  permit_params :product_id, :price, :size, :color, :amount
-  # See permitted parameters documentation:
+	permit_params :product_id, :price, :size, :color, :amount, images: []
+ 
+	# See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # permit_params :list, :of, :attributes, :on, :model
@@ -15,11 +16,12 @@ ActiveAdmin.register ConcreteProduct do
 
   form do |f|
     f.inputs do
-      f.input :product
+			f.input :product
       f.input :price
       f.input :size
       f.input :color
-      f.input :amount
+			f.input :amount
+			f.input :images, as: :file, input_html: { multiple: true }
     end
     f.actions
   end
