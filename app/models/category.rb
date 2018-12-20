8dc82bb26c76f26d60c2eaccf56d17e3370901ec
chevrette <denkiewicz.karolina@gmail.com>
2018-12-20 03:12:16 +0100

    Run rubocop

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,19 +2,19 @@ class Category < ApplicationRecord
   has_many :products
   has_many :child_categories, class_name: 'Category', foreign_key: 'parent'
 
-	def all_products(product_name: nil, **kwargs)
+  def all_products(product_name: nil, **_kwargs)
     result = []
     child_categories.each do |child|
       result += child.all_products(product_name: product_name)
-		end
-		if product_name
-			result + products.where('name ILIKE ?', "%#{product_name}%")
-		else
-			result + products
-		end
+    end
+    if product_name
+      result + products.where('name ILIKE ?', "%#{product_name}%")
+    else
+      result + products
+    end
   end
 
-	def all_concrete_products(search_phrase: nil, **kwargs)
+  def all_concrete_products(search_phrase: nil, **_kwargs)
     result = []
     all_products(product_name: search_phrase).each do |product|
       result += product.all_concrete_products
