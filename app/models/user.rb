45abba318f8639f776d4e398353e0d0b5c17b1f8
Adam <adam.kufel2@gmail.com>
2019-01-07 17:30:37 +0100

    acc_management panel extended

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+  validate :password_complexity
+
+  def password_complexity
+    return if password =~ /\A(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,12}\z/
+    if !password.blank?
+      errors.add :password, 'length should be 8-12 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+    end
+  end
+  
 end
