8480c8ad224f29a862e0209cf35db1496ea48d64
chevrette <denkiewicz.karolina@gmail.com>
2019-01-07 21:39:42 +0100

    Fix issues from merge request

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,6 @@
 
 class CategoriesController < ApplicationController
   def show
-    @query_params = request.query_parameters
     @child_categories = find_child_categories(params[:id])
     @category = Category.find(params[:id])
   end
